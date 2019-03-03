0204e6a4dd87e9a570086396e6eeeea12ae55a9f
Vladimir <alfa0724@mail.ru>
2019-03-03 11:27:58 +0400

    chapter04

---
+++ src/main/java/com/savenko/javacore/chapter04/IncDec.java
@@ -0,0 +1,21 @@
+package com.savenko.javacore.chapter04;
+
+public class IncDec {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c, d;
+
+        c = ++b;
+        d = a++;
+        c++;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+
+    }
+
+
+
+}
---
+++ src/main/java/com/savenko/javacore/chapter04/Ternary.java
@@ -0,0 +1,18 @@
+package com.savenko.javacore.chapter04;
+// применение тернарной опрации  ?
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+        i = 10;
+        k = i < 0 ? - i : i; // получить абсолютное значение переменной i
+        System.out.print("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+
+        i = -10;
+        k = i < 0 ? - i : i; // получить абсолютное значение переменной i
+
+        System.out.print("Абсолютное значение ");
+        System.out.println(i + " равно " + k);
+
+    }
+}
